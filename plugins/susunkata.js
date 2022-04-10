fa650b0d9e6996934a94f7ed890f73f812e75027
Putradindra <96910925+Putradindra@users.noreply.github.com>
2022-04-10 20:51:38 +0700

    Update susunkata.js

--- plugins/susunkata.js
+++ plugins/susunkata.js
@@ -2,37 +2,35 @@ let fetch = require('node-fetch')
 
 let timeout = 120000
 let poin = 500
-let handler = async (m, { conn, usedPrefix }) => {
+let handler = async (m, { conn, usedPrefix, command }) => {
     conn.susunkata = conn.susunkata ? conn.susunkata : {}
     let id = m.chat
     if (id in conn.susunkata) {
         conn.reply(m.chat, 'Masih ada soal belum terjawab di chat ini', conn.susunkata[id][0])
         throw false
     }
-    let res = await fetch('https://raw.githubusercontent.com/BochilTeam/database/master/games/susunkata.json')
-    if (!res.ok) throw eror
-    let data = await res.json()
-    let json = data[Math.floor(Math.random() * data.length)]
+    let res = await fetch('http://zekais-api.herokuapp.com/susunkata')
+    let json = await res.json()
     let caption = `
-${json.soal}
-*🏷️ Tipe:* ${json.tipe}
-
-*⏰ Timeout:* ${(timeout / 1000).toFixed(2)} detik
-*🎁 Bonus:* ${poin} XP
-*🔎 Hint:* ${usedPrefix}suka
+Timeout *${(timeout / 1000).toFixed(2)} detik*
+Ketik ${usedPrefix}wa untuk bantuan
+Bonus: ${poin} XP
 `.trim()
-    conn.susunkata[id] = [
-        await conn.sendButton(m.chat, caption, `${wm}`, 'Bantuan', '.suka'),
+    conn.tebakanime[id] = [
+        await conn.sendButtonImg(m.chat, await (await fetch(json.image)).buffer(), caption, wm, 'BANTUAN', '.wa', m),
         json, poin,
         setTimeout(async () => {
-            if (conn.susunkata[id]) await conn.sendButton(m.chat, `Waktu habis!\nJawabannya adalah *${json.jawaban}*`, `${wm}`, 'Susun Kata', '.susunkata')
+            if (conn.susunkata[id]) await conn.sendButton(m.chat, `Waktu habis!\nJawabannya adalah *${json.name}*`, '© nullbot', 'Susun Kata', `${usedPrefix + command}`, m)
             delete conn.susunkata[id]
         }, timeout)
     ]
 }
 handler.help = ['susunkata']
 handler.tags = ['game']
-handler.command = /^susunkata/i
+handler.command = /^susunkata$/i
+
+module.exports = handler
+let wm = global.botwm
 
 module.exports = handler
 
