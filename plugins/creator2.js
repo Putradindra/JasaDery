e829dd017b46b34f8d37b0426263e269cc11b74a
Putradindra <96910925+Putradindra@users.noreply.github.com>
2022-04-17 11:03:18 +0700

    Update creator2.js

--- plugins/creator2.js
+++ plugins/creator2.js
@@ -7,7 +7,7 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
       "vcard": "BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:Owner NullBot\nitem1.TEL;waid=6289504575881:6289504575881\nitem1.X-ABLabel:📍 Creator\nitem2.EMAIL;type=INTERNET:Veteran_Putra@gmail.com\nitem2.X-ABLabel:💌 Email\nitem3.URL:https://hardianto.xyz/\nitem3.X-ABLabel:📮 Rest Api\nitem4.ADR:;;🇮🇩 Indonesia;;;;\nitem4.X-ABADR:ac\nitem4.X-ABLabel:🌍 Region | Otaku 🇯🇵\nitem5.X-ABLabel:───────[ Null Bot ]───────\nEND:VCARD"
   }, {
     "displayName": "Owner Cadangan",
-      "vcard": "BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:Owner Cadangan\nitem1.TEL;waid=6285879150538:6285879150538\nitem1.X-ABLabel:📍 Whatsapp\nitem2.EMAIL;type=INTERNET:Veteran_Boy@gmail.com\nitem2.X-ABLabel:💌 Email\nitem3.URL:https://hardianto.xyz/\nitem3.X-ABLabel:📮 Rest Api\nitem4.ADR:;;🇮🇩 Indonesia;;;;\nitem4.X-ABADR:ac\nitem4.X-ABLabel:🌍 Region | Otaku 🇯🇵\nitem5.X-ABLabel:───────[ Null Bot ]───────\nEND:VCARD"
+      "vcard": "BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:Owner DeryBot\nitem1.TEL;waid=6285267218151:6285267218151\nitem1.X-ABLabel:📍 Whatsapp\nitem2.EMAIL;type=INTERNET:Veteran_Dery@gmail.com\nitem2.X-ABLabel:💌 Email\nitem3.URL:https://hardianto.xyz/\nitem3.X-ABLabel:📮 Rest Api\nitem4.ADR:;;🇮🇩 Indonesia;;;;\nitem4.X-ABADR:ac\nitem4.X-ABLabel:🌍 Region | Otaku 🇯🇵\nitem5.X-ABLabel:───────[ Dery Bot ]───────\nEND:VCARD"
     }]
   }, MessageType.contactsArray, { quoted: m })
 }
